c5bba48af43bca2fc499b39029f2a25b1030ead2
steve <steve@ampliostudios.com>
2009-05-29 09:24:27 -0600

    temporarily adding DictionarySearchView due to reference in project file.  It will be deleted soon

---
+++ rdict-iphone/Classes/DictionarySearchView.h
@@ -0,0 +1,16 @@
+//
+//  DictionarySearchPaneController.h
+//  RDict
+//
+//  Created by Stephen Bodnar on 28/05/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface DictionarySearchView : UIWebView {
+
+}
+
+@end
