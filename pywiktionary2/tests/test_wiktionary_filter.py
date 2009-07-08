41506dbca52a90d3f261bb9f8a56c4fa2074cf77
jaewoo <jaewoo@ampliostudios.com>
2009-07-08 22:26:37 +0900

    almost done with wiktionary filter

--- pywiktionary2/tests/test_wiktionary_filter.py
+++ pywiktionary2/tests/test_wiktionary_filter.py
@@ -19,6 +19,11 @@ class WiktionaryFilterTest( unittest.TestCase ):
         self.assertTrue( None != content )
         
         filter.executeSoupFilters( content )
+        
+        f = open( '/tmp/get.html', 'w' )
+        f.write( str( content ) )
+        f.close()
+        os.system( "open /tmp/get.html" ) 
 #        filter.executeRegexFilters()
 #        out = filter.html()
 #        
@@ -33,13 +38,25 @@ class SoupFilterTest( unittest.TestCase ):
         self.filter.soup_filter_removeTitleInA( content )
         self.assertEquals( '<a href="http://test">link</a>', str(content) )
 
-    def testRemoveEditSectionSpan(self ):
-        content = BeautifulSoup( '<span class="editsection">edit</span><div>test</div>' )
-        self.filter.soup_filter_removeEditSectionSpan( content )
-        self.assertEquals( '<div>test</div>', str(content) )
+#    def testRemoveEditSectionSpan(self ):
+#        content = BeautifulSoup( '<span class="editsection">edit</span><div>test</div>' )
+#        self.filter.soup_filter_removeEditSectionSpan( content )
+#        self.assertEquals( '<div>test</div>', str(content) )
+
+    def testPullUpHeadSpan(self):
+        content = BeautifulSoup( '<h3><span class="editsection">[<a href="/w/index.php?title=get&amp;action=edit&amp;section=3" title="Edit section: Etymology 1">edit</a>]</span> <span class="mw-headline">Etymology 1</span></h3>' )
+        self.filter.pullUpHeadSpanContent( content )
+        self.assertEquals( '<h3 class="head">Etymology 1</h3>', str(content) )
 
         
-        
+    def testRemoveUnnecessaryElements( self ):    
+        content = BeautifulSoup( '''<div id="rank" title="Word frequency based on Project Gutenberg corpus"><a href="/wiki/away">away</a>&nbsp;«&nbsp;<a href="/wiki/against">against</a>&nbsp;«&nbsp;<a href="/wiki/though">though</a>&nbsp;«&nbsp;<a href="/wiki/Wiktionary:Frequency_lists">#149:&nbsp;get</a>&nbsp;»&nbsp;<a href="/wiki/eyes">eyes</a>&nbsp;»&nbsp;<a href="/wiki/hand">hand</a>&nbsp;»&nbsp;<a href="/wiki/young">young</a></div>                    
+<div class="infl-table"><table border="0" width="100%"></div>
+<hr>
+''')
+        self.filter.soup_filter_removeUnnecessaryElements( content )
+        self.assertEquals( '', str(content).strip() )
+    
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
--- pywiktionary2/wiktionary_filter.py
+++ pywiktionary2/wiktionary_filter.py
@@ -4,22 +4,65 @@
 # Created at Jul 7, 2009 by evacuee
 
 from BeautifulSoup import BeautifulSoup
+from cStringIO import StringIO
 
 class WiktionaryFilter:
     
     def findContent( self, wiktionaryPage ):
         page = BeautifulSoup( wiktionaryPage )
-        return page.find( 'div', id='bodyContent' )
-    
+        bodyContent = page.find( 'div', id = 'bodyContent' )
+        self.pullUpHeadSpanContent( bodyContent )
+        
+        buffer = StringIO()
+#        buffer.write( '<!DOCTYPE html><html lang="en"><head><meta charset=utf-8></head><body>\n')
+        h2s = bodyContent.findAll( 'h2', {'class':'head'}  )
+        
+        englishHead = h2s[0]
+        otherLangHead = None
+        if len( h2s ) > 1:
+            otherLangHead = h2s[1]
+            
+        g = englishHead.nextSiblingGenerator()
+        n = g.next()
+        while otherLangHead != n:
+            buffer.write( str( n ) )
+            buffer.write( '\n' )
+            n = g.next()
+#        buffer.write( '</body></html>' )
+        buffer.flush()
+        
+        return BeautifulSoup( buffer.getvalue(), fromEncoding='utf-8' )
+
+    def pullUpHeadSpanContent( self, content ):
+        headSpans = content.findAll('span', {'class':'mw-headline'} )
+        for headSpan in headSpans:
+            parent = headSpan.parent
+            parent['class'] = 'head' 
+            parent.contents = headSpan.contents
+                    
     def executeSoupFilters( self, content ):
         for d in dir( self ):
             if d.startswith( 'soup_filter_' ):
-                getattr( self, d )( content)
+                getattr( self, d )( content )
         
     def soup_filter_removeTitleInA( self, content ):
         for link in content.findAll( 'a' ):
             del( link['title'] )
         
-    def soup_filter_removeEditSectionSpan(self, content ):
-        editSectionSpan = content.findAll( 'span', {'class':'editsection'} )
-        [ ess.extract() for ess in editSectionSpan ]
+    def soup_filter_removeUnnecessaryElements( self, content ):
+        ELEMENTS = { 
+            'span' : [ 
+                {'class':'editsection'},
+            ],
+            'div' : [ 
+                {'id':'rank'},
+                {'class':'infl-table'},
+            ],
+        } 
+        
+        for tag in ELEMENTS.keys():
+            attrs = ELEMENTS[tag]
+            for attr in attrs:
+                soups = content.findAll( tag, attr )
+                [ s.extract() for s in soups ]
+            
