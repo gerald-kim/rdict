b85cda9d0e956fb619b5355da688ae1548f70713
steve <steve@ampliostudios.com>
2009-07-19 17:24:54 -0600

    removed class FlashCardViewWrapper.

--- rdict-android/src/com/amplio/rdict/more/FlashcardAdapter.java
+++ rdict-android/src/com/amplio/rdict/more/FlashcardAdapter.java
@@ -7,7 +7,6 @@ import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.amplio.rdict.R;
