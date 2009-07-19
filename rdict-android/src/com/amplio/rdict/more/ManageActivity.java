1cd673a689cb3f8d970087e9e30ef8f38afd7faf
steve <steve@ampliostudios.com>
2009-07-19 12:15:28 -0600

    manage screen loads unescaped version of a card.  need to use the layout to make it look nice

--- rdict-android/src/com/amplio/rdict/more/ManageActivity.java
+++ rdict-android/src/com/amplio/rdict/more/ManageActivity.java
@@ -1,22 +1,59 @@
 package com.amplio.rdict.more;
 
-import com.amplio.rdict.R;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
 
-public class ManageActivity extends Activity{
+import com.amplio.rdict.R;
+import com.amplio.rdict.RDictActivity;
+import com.amplio.rdict.review.Card;
+import com.amplio.rdict.review.CardSetManager;
+import com.db4o.ObjectSet;
+
+public class ManageActivity extends Activity implements OnItemClickListener {
+	
+	CardSetManager cardMgr = null;
+	
+	ListView cardList = null;
 	
 	@Override
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
 		setContentView(R.layout.manage);
+		
+		this.cardMgr = new CardSetManager(RDictActivity.db);
+		
+		this.cardList = (ListView) findViewById(R.id.card_list_view);
+		this.cardList.setOnItemClickListener(this);
 	}
 	
 	public void onResume() {
-		// load cards
-		// for each card
-		//		show a flashcard to user
+		super.onResume();
+		String prefix = "";
+		ObjectSet cards = this.cardMgr.loadCardsByPrefix(prefix);
+		
+		ArrayAdapter<String> aa = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1);
+		
+		System.out.println(cards.size());
+		
+		for(int i=0; i < cards.size(); i++) {
+			//		show a flashcard to user
+			Card card = (Card) cards.get(i);
+			
+			aa.add(card.question + "\n" + card.answer);
+		}
+
+		this.cardList.setAdapter(aa);
+		aa.notifyDataSetChanged();
+	}
+
+	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
