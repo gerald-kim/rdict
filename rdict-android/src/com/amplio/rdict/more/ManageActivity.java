d67e6d45b298a2fa95dc0accd26d284a63cfb06b
steve <steve@ampliostudios.com>
2009-07-18 18:33:00 -0600

    added infrastructure for the More tab functionality, but all pages are nonfunctional

---
+++ rdict-android/src/com/amplio/rdict/more/ManageActivity.java
@@ -0,0 +1,16 @@
+package com.amplio.rdict.more;
+
+import com.amplio.rdict.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ManageActivity extends Activity{
+	
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		setContentView(R.layout.manage);
+	}
+
+}
---
+++ rdict-android/src/com/amplio/rdict/more/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.amplio.rdict.more;
+
+import com.amplio.rdict.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingsActivity extends Activity{
+	
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		setContentView(R.layout.settings);
+	}
+
+}
