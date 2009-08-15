6a78e3e34368c46d9f67914cd2578bd1a3640999
steve <steve@ampliostudios.com>
2009-08-15 04:59:28 -0600

    added some help screens to the flashcard exercises

--- rdict-android/src/main/java/com/amplio/rdict/review/ReviewExerciseBackButtonsViewWrapper.java
+++ rdict-android/src/main/java/com/amplio/rdict/review/ReviewExerciseBackButtonsViewWrapper.java
@@ -1,5 +1,6 @@
 package com.amplio.rdict.review;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -17,6 +18,8 @@ public class ReviewExerciseBackButtonsViewWrapper implements OnClickListener {
 	Button m_hardButton = null;
 	Button m_iForgotButton = null;
 	
+	Button m_helpButton = null;
+	
 	public ReviewExerciseBackButtonsViewWrapper(Context context, FlashCardActivity a) {		
 		this.m_view = View.inflate(context, R.layout.review_exercise_buttons_back, null);
 		
@@ -29,25 +32,37 @@ public class ReviewExerciseBackButtonsViewWrapper implements OnClickListener {
 		this.m_iForgotButton = (Button) this.m_view.findViewById(R.id.i_forgot_button);
 		this.m_iForgotButton.setOnClickListener(this);
 		
+		this.m_helpButton = (Button) this.m_view.findViewById(R.id.back_help_button);
+		this.m_helpButton.setOnClickListener(this);
+		
 		this.m_a = a;
 	}
 
 	public void onClick(View v) {
 		v.setPressed(false);
 	    
-		FlashCardActivity.m_exerciseMgr.userPressedAnEasinessButton(this.getGradeByButton(v));
-	    RDictActivity.c_cardSetManager.save(FlashCardActivity.m_exerciseMgr.getCard());
-	    
-	    RDictActivity.RDICT_ACTIVITY.updateReviewTabIndicator();
-	    
-	    FlashCardActivity.m_exerciseMgr.next();
-		
-	    if(ReviewExerciseManager.STATE_USER_FINISHED_EXERCISE != FlashCardActivity.m_exerciseMgr.getState()) {
-	    	this.m_a.drawDisplay();
-		}
-		else{
-			RDictActivity.c_statisticsManager.saveOrUpdateCardStackStatistics();
-			this.m_a.finish();
+		if(v != this.m_helpButton) {
+			FlashCardActivity.m_exerciseMgr.userPressedAnEasinessButton(this.getGradeByButton(v));
+		    RDictActivity.c_cardSetManager.save(FlashCardActivity.m_exerciseMgr.getCard());
+		    
+		    RDictActivity.RDICT_ACTIVITY.updateReviewTabIndicator();
+		    
+		    FlashCardActivity.m_exerciseMgr.next();
+			
+		    if(ReviewExerciseManager.STATE_USER_FINISHED_EXERCISE != FlashCardActivity.m_exerciseMgr.getState()) {
+		    	this.m_a.drawDisplay();
+			}
+			else{
+				RDictActivity.c_statisticsManager.saveOrUpdateCardStackStatistics();
+				this.m_a.finish();
+			}
+	    }
+	    else {
+			new AlertDialog.Builder(this.m_a)
+			.setTitle("Help")
+			.setMessage("How easy was it to remember the word?\n\nTell RDict by pressing one of the buttons.")
+			.setNeutralButton("Ok", null)
+			.show();
 		}
     }
 	
--- rdict-android/src/main/java/com/amplio/rdict/review/ReviewExerciseFrontButtonsViewWrapper.java
+++ rdict-android/src/main/java/com/amplio/rdict/review/ReviewExerciseFrontButtonsViewWrapper.java
@@ -1,5 +1,6 @@
 package com.amplio.rdict.review;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,22 +13,34 @@ public class ReviewExerciseFrontButtonsViewWrapper implements OnClickListener {
 	private FlashCardActivity m_a = null;
 	private View m_view = null;
 	Button m_viewAnswerButton = null;
+	Button m_helpButton = null;
 	
 	public ReviewExerciseFrontButtonsViewWrapper(Context context, FlashCardActivity a) {		
 		this.m_context = context;
 		this.m_view = View.inflate(m_context, R.layout.review_exercise_buttons_front, null);
 		this.m_viewAnswerButton = (Button) this.m_view.findViewById(R.id.view_answer_button);
 		this.m_viewAnswerButton.setOnClickListener(this);
+		this.m_helpButton = (Button) this.m_view.findViewById(R.id.front_help_button);
+		this.m_helpButton.setOnClickListener(this);
 		
 		this.m_a = a;
 	}
 
 	public void onClick(View v) {
-		FlashCardActivity.m_exerciseMgr.userPressedViewAnswerButton();
-		
-		v.setPressed(false);
-		
-	    m_a.drawDisplay();
+		if(v == this.m_viewAnswerButton) {
+			FlashCardActivity.m_exerciseMgr.userPressedViewAnswerButton();
+			
+			v.setPressed(false);
+			
+		    m_a.drawDisplay();
+		}
+		else {
+			new AlertDialog.Builder(this.m_a)
+			.setTitle("Help")
+			.setMessage("Can you remember this word?  Think about the definition.\n\nWhen you remember, of if you decide you can't remember, push the 'View Answer' button.")
+			.setNeutralButton("Ok", null)
+			.show();
+		}
     }
 
 	public View getView() {
