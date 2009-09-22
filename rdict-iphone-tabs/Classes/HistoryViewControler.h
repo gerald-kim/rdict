36d2f49fb085517513ca05aa8c0372fd8c70c254
jaewoo <jaewoo@ampliostudios.com>
2009-09-22 09:01:40 +0900

    added HistoryView

---
+++ rdict-iphone-tabs/Classes/HistoryViewControler.h
@@ -0,0 +1,16 @@
+//
+//  HistoryViewControler.h
+//  RDict
+//
+//  Created by Jaewoo Kim on 9/16/09.
+//  Copyright 2009 ampliostudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface HistoryViewControler : UIViewController {
+
+}
+
+@end
