169d4e6454ea3fd5c4c4203d9669c84170a12dc9
jaewoo <jaewoo@ampliostudios.com>
2009-12-16 08:14:34 +0900

    removed old iphone project

--- rdict-iphone/Classes/Person.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "SQLitePersistentObject.h"
-
-@interface Person : SQLitePersistentObject {
-	NSString *name;
-}
-@property (nonatomic, retain) NSString * name;
-@end
