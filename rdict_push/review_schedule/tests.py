2f024c5644b004d6a0628f1d8e043853b83802a4
jaewoo <jaewoo@ampliostudios.com>
2010-05-06 08:30:18 +0900

    push server

---
+++ rdict_push/review_schedule/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+class ScheduleModelTest(TestCase):
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEquals(1 + 1, 2)
+
+
+
